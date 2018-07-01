bf72d0cc92ce8bf273e895bef5ec6c52dbb093c0
ivan Jankovic <jankoemail@gmail.com>
2018-06-30 22:06:13 -0400

    first commit

--- js/engine.js
+++ js/engine.js
@@ -20,7 +20,7 @@ var Engine = (function(global) {
      */
     var doc = global.document,
         win = global.window,
-        canvas = doc.createElement('canvas'),
+        canvas = docdocument.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         lastTime;
 
